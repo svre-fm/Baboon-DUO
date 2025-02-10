29901fae1c567cbf9e18ec6e70a41fddc759976e
svre-fm <Firmoioi96@gmail.com>
2025-02-11 02:08:40 +0700

    make  state & 2 commander

--- src/command.cpp
+++ src/command.cpp
@@ -12,86 +12,114 @@ const int screenWidth = 1200;
 const int screenHeight = 800;
 
 vector<string> commands = {"Up", "Down", "Left", "Right"};
+vector<string> oppositeCommands = {"Down", "Up", "Right", "Left"};
 string currentCommand;
 bool gameOver = false;
-bool p1Pressed = false; 
-bool p2Pressed = false; 
-bool p1correct = false; 
-bool p2correct = false; 
-bool waitfornextcommand = false; 
-int p1score = 0;
-int p2score = 0;
-float gametime = 150.0f;
+bool p1Pressed = false;
+bool p2Pressed = false;
+bool waitfornextcommand = false;
+bool isOppositeCommand = false;
+int scorep1 = 0;
+int scorep2 = 0;
+int stage = 1;
+float stagetime = 30.0f;
 float txttime = 0.0f;
-float waitTime = 0.0f; 
+float waitTime = 0.0f;
 
 void generateCommand() {
     txttime = 5.0f; 
-    currentCommand = commands[rand() % commands.size()];
+    if(stage == 1){
+        isOppositeCommand = false;
+        currentCommand = commands[rand() % commands.size()];
+    }else if(stage == 2){
+        isOppositeCommand = true;
+        int index = rand() % commands.size();
+        currentCommand = oppositeCommands[index];
+    }else{
+        isOppositeCommand = (rand()%2 == 0);
+        if(isOppositeCommand){
+            int index = rand() % commands.size();
+            currentCommand = oppositeCommands[index];
+        }else{
+            currentCommand = commands[rand() % commands.size()];
+        }
+    }
     p1Pressed = false;
     p2Pressed = false;
-    p1correct = false;
-    p2correct = false;
     waitfornextcommand = false;
 }
 
 void checkInput() {
     if (!p1Pressed) {
-        if (currentCommand == "Up" && IsKeyPressed(KEY_UP)) {
-            p1correct = true;
-            p1Pressed = true;
-            p1score++;
-        }
-        if (currentCommand == "Down" && IsKeyPressed(KEY_DOWN)) {
-            p1correct = true;
-            p1Pressed = true;
-            p1score++;
-        }
-        if (currentCommand == "Left" && IsKeyPressed(KEY_LEFT)) {
-            p1correct = true;
-            p1Pressed = true;
-            p1score++;
-        }
-        if (currentCommand == "Right" && IsKeyPressed(KEY_RIGHT)) {
-            p1correct = true;
-            p1Pressed = true;
-            p1score++;
-        }
-        if ((currentCommand != "Up" && IsKeyPressed(KEY_UP)) ||
-            (currentCommand != "Down" && IsKeyPressed(KEY_DOWN)) ||
-            (currentCommand != "Left" && IsKeyPressed(KEY_LEFT)) ||
-            (currentCommand != "Right" && IsKeyPressed(KEY_RIGHT))) {
-            p1Pressed = true; 
+        if (isOppositeCommand) {
+            if (currentCommand == "Down" && IsKeyPressed(KEY_UP)) {
+                p1Pressed = true;
+                scorep1++; 
+            } else if (currentCommand == "Up" && IsKeyPressed(KEY_DOWN)) {
+                p1Pressed = true;
+                scorep1++; 
+            } else if (currentCommand == "Right" && IsKeyPressed(KEY_LEFT)) {
+                p1Pressed = true;
+                scorep1++; 
+            } else if (currentCommand == "Left" && IsKeyPressed(KEY_RIGHT)) {
+                p1Pressed = true;
+                scorep1++; 
+            } else if (IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_RIGHT)) {
+                p1Pressed = true; 
+            }
+        } else {
+            
+            if (currentCommand == "Up" && IsKeyPressed(KEY_UP)) {
+                p1Pressed = true;
+                scorep1++; 
+            } else if (currentCommand == "Down" && IsKeyPressed(KEY_DOWN)) {
+                p1Pressed = true;
+                scorep1++; 
+            } else if (currentCommand == "Left" && IsKeyPressed(KEY_LEFT)) {
+                p1Pressed = true;
+                scorep1++; 
+            } else if (currentCommand == "Right" && IsKeyPressed(KEY_RIGHT)) {
+                p1Pressed = true;
+                scorep1++;
+            } else if (IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_RIGHT)) {
+                p1Pressed = true; 
+            }
         }
     }
-
     if (!p2Pressed) {
-        if (currentCommand == "Up" && IsKeyPressed(KEY_W)) {
-            p2correct = true;
-            p2Pressed = true;
-            p2score++; 
-        }
-        if (currentCommand == "Down" && IsKeyPressed(KEY_S)) {
-            p2correct = true;
-            p2Pressed = true;
-            p2score++; 
-        }
-        if (currentCommand == "Left" && IsKeyPressed(KEY_A)) {
-            p2correct = true;
-            p2Pressed = true;
-            p2score++; 
-        }
-        if (currentCommand == "Right" && IsKeyPressed(KEY_D)) {
-            p2correct = true;
-            p2Pressed = true;
-            p2score++; 
-        }
-
-        if ((currentCommand != "Up" && IsKeyPressed(KEY_W)) ||
-            (currentCommand != "Down" && IsKeyPressed(KEY_S)) ||
-            (currentCommand != "Left" && IsKeyPressed(KEY_A)) ||
-            (currentCommand != "Right" && IsKeyPressed(KEY_D))) {
-            p2Pressed = true; 
+        if (isOppositeCommand) {
+            if (currentCommand == "Down" && IsKeyPressed(KEY_W)) {
+                p2Pressed = true;
+                scorep2++; 
+            } else if (currentCommand == "Up" && IsKeyPressed(KEY_S)) {
+                p2Pressed = true;
+                scorep2++; 
+            } else if (currentCommand == "Right" && IsKeyPressed(KEY_A)) {
+                p2Pressed = true;
+                scorep2++; 
+            } else if (currentCommand == "Left" && IsKeyPressed(KEY_D)) {
+                p2Pressed = true;
+                scorep2++; 
+            } else if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_S) || IsKeyPressed(KEY_A) || IsKeyPressed(KEY_D)) {
+                p2Pressed = true;
+            }
+        } else {
+            
+            if (currentCommand == "Up" && IsKeyPressed(KEY_W)) {
+                p2Pressed = true;
+                scorep2++; 
+            } else if (currentCommand == "Down" && IsKeyPressed(KEY_S)) {
+                p2Pressed = true;
+                scorep2++; 
+            } else if (currentCommand == "Left" && IsKeyPressed(KEY_A)) {
+                p2Pressed = true;
+                scorep2++; 
+            } else if (currentCommand == "Right" && IsKeyPressed(KEY_D)) {
+                p2Pressed = true;
+                scorep2++; 
+            } else if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_S) || IsKeyPressed(KEY_A) || IsKeyPressed(KEY_D)) {
+                p2Pressed = true;
+            }
         }
     }
 }
@@ -104,26 +132,30 @@ void playcommand() {
 
     while (!WindowShouldClose()) {
         if (!gameOver) {
-            gametime -= GetFrameTime(); 
-            txttime -= GetFrameTime();  
+            stagetime -= GetFrameTime();
+            txttime -= GetFrameTime();
 
+            if(stagetime <= 0){
+                stage++;
+                if(stage > 3){
+                    gameOver = true;
+                }else{
+                    stagetime = 30.0f;
+                    generateCommand();
+                }
+
+            }
             if (!waitfornextcommand) {
                 checkInput();
-
-                
                 if ((p1Pressed && p2Pressed) || txttime <= 0) {
                     waitfornextcommand = true;
-                    waitTime = 3.0f; 
+                    waitTime = 3.0f;
                 }
             } else {
-                waitTime -= GetFrameTime(); 
+                waitTime -= GetFrameTime(); // นับเวลาถอยหลังสำหรับรอ 3 วินาที
                 if (waitTime <= 0) {
-                    generateCommand(); 
+                    generateCommand(); // สุ่มคำสั่งใหม่
                 }
-            }
-
-            if (gametime <= 0) {
-                gameOver = true;
             }
         }
 
@@ -131,23 +163,27 @@ void playcommand() {
         ClearBackground(RAYWHITE);
 
         if (gameOver) {
-            DrawText("Game Over! Press R to Restart", 200, 250, 30, RED);
-            if (IsKeyPressed(KEY_R)) {
-                p1score = 0;
-                p2score = 0;
-                gametime = 150.0f; 
-                gameOver = false;
-                generateCommand();
+            if(scorep1 > scorep2){
+                DrawText("Palyer 1 win", 300, 200, 30, BLACK);
+            }else if(scorep2 > scorep1){
+                DrawText("Palyer 2 win", 300, 200, 30, BLACK);
+            }else if(scorep1 == scorep2){
+                DrawText("Draw", 300, 200, 30, BLACK);
             }
+            
         } else {
             if (waitfornextcommand) {
                 DrawText("Waiting for next command...", 300, 200, 30, BLACK);
             } else {
-                DrawText(TextFormat("Command: %s", currentCommand.c_str()), 300, 200, 30, BLACK);
+                if (isOppositeCommand) {
+                DrawText(TextFormat("Command: %s", currentCommand.c_str()), 300, 200, 30, RED);
+                }else{
+                DrawText(TextFormat("Command: %s", currentCommand.c_str()), 300, 200, 30, BLUE); 
+                }
             }
             DrawText(TextFormat("Time: %.1f", txttime), 350, 300, 30, BLACK);
-            DrawText(TextFormat("Score P1: %d", p1score), 200, 400, 30, BLUE);
-            DrawText(TextFormat("Score P2: %d", p2score), 400, 400, 30, BLUE);
+            DrawText(TextFormat("Score P1: %d", scorep1), 400, 400, 30, BLUE);
+            DrawText(TextFormat("Score P2: %d", scorep2), 200, 400, 30, BLUE);
         }
 
         EndDrawing();
